cc7a17ca0e4b321eb05d882b4f0340c85b8c743a
Bob Deckard <robert_deckard@homedepot.com>
2020-03-20 11:33:45 -0500

    early endpoints

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,15 +1,42 @@
-import setText, {appendText, showWaiting, hideWaiting} from './results.mjs';
+import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
-export function get(){
-}
-
-export function getCatch(){
-}
-
-export function chain(){
+export function get() {
+  axios.get("http://localhost:3000/orders/1").then(({data}) => {
+    setText(JSON.stringify(data));
+  });
 }
 
-export function chainCatch(){
+export function getCatch() {
+  axios
+    .get("http://localhost:3000/orders/123")
+    .then(({data}) => {
+      setText(JSON.stringify(data));
+    })
+    .catch(err => setText(err));
 }
-export function final(){
+
+export function chain() {
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
+    })
+    .then(({data}) => {
+      setText(`City: ${data.city}`);
+    });
 }
+
+export function chainCatch() {
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+      axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+    .then(({data}) => {
+      setText(`City: ${data.city}`);
+    })
+    .catch(err => setText(err));
+}
+export function final() {}
